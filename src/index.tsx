d04af0818c72bae74a1d7809922ad8df7a913ff6
Nhu Son <son.nhu@eastgate-software.com>
2021-03-17 16:14:31 +0700

    add value props to input message

--- src/index.tsx
+++ src/index.tsx
@@ -28,6 +28,7 @@ type Props = {
   imagePreview?: boolean;
   zoomStep?: number;
   handleSubmit?: AnyFunction;
+  value?: string
 } & typeof defaultProps;
 
 function ConnectedWidget({
@@ -50,7 +51,8 @@ function ConnectedWidget({
   showTimeStamp,
   imagePreview,
   zoomStep,
-  handleSubmit
+  handleSubmit,
+  value
 }: Props) {
   return (
     <Provider store={store}>
@@ -75,6 +77,7 @@ function ConnectedWidget({
         imagePreview={imagePreview}
         zoomStep={zoomStep}
         handleSubmit={handleSubmit}
+        value={value}
       />
     </Provider>
   );
